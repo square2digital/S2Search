54c10aceb0fc2cd846dd1fcf8bc1fd0d4a5bad42
JGilmatin <searchengine2019@outlook.com>
2023-06-12 23:57:17 +0100

    wip

--- Cache/S2Search.CacheManager.App/src/app/Services/Providers/AzureSearchClientProvider.cs
+++ Cache/S2Search.CacheManager.App/src/app/Services/Providers/AzureSearchClientProvider.cs
@@ -9,39 +9,39 @@ using System.Threading.Tasks;
 
 namespace Services.Providers
 {
-    internal class AzureSearchClientProvider
-    {
-        private readonly IAppSettings _appSettings;
+    //internal class AzureSearchClientProvider
+    //{
+    //    private readonly IAppSettings _appSettings;
 
-        public AzureSearchDocumentsClientProvider(IAppSettings appSettings)
-        {
-            _appSettings = appSettings ?? throw new ArgumentNullException(nameof(appSettings));
-        }
+    //    public AzureSearchDocumentsClientProvider(IAppSettings appSettings)
+    //    {
+    //        _appSettings = appSettings ?? throw new ArgumentNullException(nameof(appSettings));
+    //    }
 
-        public SearchClient GetSearchClient(string endpoint, string indexName, string apiKey)
-        {
-            var searchClientFactory = GetSearchClientFactory(endpoint, indexName, apiKey);
-        }
+    //    public SearchClient GetSearchClient(string endpoint, string indexName, string apiKey)
+    //    {
+    //        var searchClientFactory = GetSearchClientFactory(endpoint, indexName, apiKey);
+    //    }
 
-        public SearchIndexClient GetIndexClient(string searchServiceName, string endpoint, string apiKey)
-        {
-            return GetSearchIndexClientFactory(endpoint, apiKey);
-        }
+    //    public SearchIndexClient GetIndexClient(string searchServiceName, string endpoint, string apiKey)
+    //    {
+    //        return GetSearchIndexClientFactory(endpoint, apiKey);
+    //    }
 
-        private SearchClient GetSearchClientFactory(string endpoint, string indexName, string apiKey)
-        {
-            return new SearchClient(new Uri(endpoint), indexName, GetAzureKeyCredential(apiKey));
-        }
+    //    private SearchClient GetSearchClientFactory(string endpoint, string indexName, string apiKey)
+    //    {
+    //        return new SearchClient(new Uri(endpoint), indexName, GetAzureKeyCredential(apiKey));
+    //    }
 
-        private SearchIndexClient GetSearchIndexClientFactory(string endpoint, string apiKey)
-        {
-            return new SearchIndexClient(new Uri(endpoint), GetAzureKeyCredential(apiKey));
-        }
+    //    private SearchIndexClient GetSearchIndexClientFactory(string endpoint, string apiKey)
+    //    {
+    //        return new SearchIndexClient(new Uri(endpoint), GetAzureKeyCredential(apiKey));
+    //    }
 
-        private AzureKeyCredential GetAzureKeyCredential(string apiKey)
-        {
-            AzureKeyCredential azureKeyCredential = new AzureKeyCredential(apiKey);
-            return azureKeyCredential;
-        }
-    }
+    //    private AzureKeyCredential GetAzureKeyCredential(string apiKey)
+    //    {
+    //        AzureKeyCredential azureKeyCredential = new AzureKeyCredential(apiKey);
+    //        return azureKeyCredential;
+    //    }
+    //}
 }
