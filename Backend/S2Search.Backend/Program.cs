b7bc04246f67606bc1f201eba90dcfc8743120e4
jonathan gilmartin <jonathangilmartin@square2digital.com>
2025-08-07 23:35:56 +0100

    Enhance configuration and service setup in Program.cs
    Updated Program.cs to include new using directives for
    options pattern and domain models. Added HttpContextAccessor
    and configured AppSettings for dependency injection.
    
    Modified appsettings.json to update search credentials
    and endpoint, reflecting a shift to a new search service
    configuration.

--- Backend/S2Search.Backend/Program.cs
+++ Backend/S2Search.Backend/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 using S2Search.Backend.Domain.Interfaces;
+using S2Search.Backend.Domain.Models;
 using S2Search.Backend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +13,12 @@ ServiceCollectionExtension.Configuration = builder.Configuration;
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddAPIServices();
+
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+builder.Services.AddSingleton<IAppSettings>(sp =>
+    sp.GetRequiredService<IOptions<AppSettings>>().Value);
 
 var app = builder.Build();
 
