91a7945a2c98495a2a1ca95bf57f4c4fbaa89bdd
jonathan gilmartin <jonathangilmartin@square2digital.com>
2025-08-22 22:51:04 +0100

    Refactor Program class and Main method structure
    Encapsulated the `Main` method within a `Program` class to improve organization. Moved `Console.WriteLine` statements for startup and shutdown into the `Main` method. Retained the `try-catch-finally` block for `IPurgeCacheProcessor` execution, ensuring proper structure within the `using` statement for the host, enhancing code readability while preserving functionality.

--- Backend/S2Search.CacheManager/Program.cs
+++ Backend/S2Search.CacheManager/Program.cs
@@ -3,27 +3,35 @@ using CacheManager.Extensions;
 using Microsoft.Extensions.Hosting;
 using Services.Interfaces.Processors;
 
-Console.WriteLine("Cache Manager Starting up...");
-
-var host = HostBuilderExtensions.CreateHostBuilder(args).Build();
-var cancelTokenSource = new CancellationTokenSource();
-
-using (host)
+namespace CacheManagerApp
 {
-    try
+    class Program
     {
-        await host.Instance<IPurgeCacheProcessor>().RunAsync(cancelTokenSource.Token);
-    }
-    catch
-    {
-        Console.WriteLine("Exception caught");
-    }
-    finally
-    {
-        //pause here to allow the logs
-        cancelTokenSource.CancelAfter(TimeSpan.FromSeconds(5));
-        await host.WaitForShutdownAsync(cancelTokenSource.Token);
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Cache Manager Starting up...");
+
+            var host = HostBuilderExtensions.CreateHostBuilder(args).Build();
+            var cancelTokenSource = new CancellationTokenSource();
+
+            using (host)
+            {
+                try
+                {
+                    await host.Instance<IPurgeCacheProcessor>().RunAsync(cancelTokenSource.Token);
+                }
+                catch
+                {
+                    Console.WriteLine("Exception caught");
+                }
+                finally
+                {
+                    cancelTokenSource.CancelAfter(TimeSpan.FromSeconds(5));
+                    await host.WaitForShutdownAsync(cancelTokenSource.Token);
+                }
+            }
+
+            Console.WriteLine("Cache Manager Shutting Down...");
+        }
     }
 }
-
-Console.WriteLine("Cache Manager Shutting Down...");
