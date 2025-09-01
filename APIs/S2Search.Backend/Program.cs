fd4bcc95a32aa992d3caa9b54f53d90a1739b8cc
jonathan gilmartin <jonathangilmartin@square2digital.com>
2025-09-01 20:54:20 +0100

    Enable Swagger UI in all environments
    Comment out development-only checks for Swagger middleware,
    allowing it to be accessible regardless of the environment.

--- APIs/S2Search.Backend/Program.cs
+++ APIs/S2Search.Backend/Program.cs
@@ -23,11 +23,14 @@ builder.Services.AddSingleton<IAppSettings>(sp =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
