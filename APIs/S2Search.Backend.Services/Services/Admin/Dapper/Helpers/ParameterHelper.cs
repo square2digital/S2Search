46e82eb1041a1bbe1d03cf3724f67fdd8146a717
jonathan gilmartin <jonathangilmartin@square2digital.com>
2025-09-20 14:51:52 +0100

    Fix formatting in ParseParameters method
    Removed an extra space in the assignment of `dynamicParameters`
    within the `ParseParameters` method in `ParameterHelper.cs`.
    This change improves code readability and adheres to standard
    formatting practices without affecting functionality.

--- APIs/S2Search.Backend.Services/Services/Admin/Dapper/Helpers/ParameterHelper.cs
+++ APIs/S2Search.Backend.Services/Services/Admin/Dapper/Helpers/ParameterHelper.cs
@@ -19,7 +19,7 @@ namespace S2Search.Backend.Services.Services.Admin.Dapper.Helpers
         {
             ValidateParameters(parameters);
 
-            var dynamicParameters = ConvertToDynamicParameters(procedureName, parameters);
+           var dynamicParameters = ConvertToDynamicParameters(procedureName, parameters);
 
             return dynamicParameters;
         }
