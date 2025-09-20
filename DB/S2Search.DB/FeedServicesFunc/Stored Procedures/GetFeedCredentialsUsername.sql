a1dc231b9b5df12613a7bc597aa0fe28e6a90784
jonathan gilmartin <jonathangilmartin@square2digital.com>
2025-09-20 14:31:19 +0100

    Add GetFeedCredentialsUsername stored procedure
    A new stored procedure `GetFeedCredentialsUsername` was added to the `FeedServicesFunc` schema. This procedure retrieves the first record from the `FeedCredentials` table based on a given `SearchIndexId`.
    
    The `S2Search.DB.sqlproj` file was updated to include the new stored procedure file as a build item, ensuring it is part of the database project and deployment process.

---
+++ DB/S2Search.DB/FeedServicesFunc/Stored Procedures/GetFeedCredentialsUsername.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [FeedServicesFunc].[GetFeedCredentialsUsername]
+(
+	@SearchIndexId uniqueidentifier
+)
+AS
+
+BEGIN
+
+SELECT TOP 1
+fc.SearchIndexId,
+fc.Username,
+fc.CreatedDate,
+fc.ModifiedDate
+FROM dbo.FeedCredentials fc
+WHERE fc.SearchIndexId = @SearchIndexId
+
+END
