2501c328eba2b0519058f306efe30f0c1b2345d6
jonathan gilmartin <jonathangilmartin@square2digital.com>
2025-10-20 22:09:46 +0100

    Add Domain and Services projects to solution
    Introduced two new projects, `Domain` and `Services`, to the `SearchInsights.sln` solution. Updated the solution file to include these projects and defined their `Debug` and `Release` build configurations.
    
    The `Domain` project includes:
    - Constants for connection strings, function names, storage queues, stored procedures, time categories, and search insights.
    - Models for search facets, data points, and messages.
    - Targets `.NET 9.0` with `ImplicitUsings` and `Nullable` enabled.
    
    The `Services` project is a placeholder for future service-related implementations and also targets `.NET 9.0`.
    
    These changes establish a modular architecture for managing search insights.

---
+++ AzureFunctions/Domain/Constants/StoredProcedures.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Constants
+{
+    public static class StoredProcedures
+    {
+        public const string AddDataPoints = "[SearchInsightsFunc].[AddDataPoints]";
+        public const string AddSearchRequest = "[SearchInsightsFunc].[AddSearchRequest]";
+    }
+}
---
+++ AzureFunctions/Domain/Constants/TimeOfDay.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Constants
+{
+    public class TimeOfDay
+    {
+        public const string Morning = "Morning";
+        public const string Afternoon = "Afternoon";
+        public const string Evening = "Evening";
+        public const string Night = "Night";
+    }
+}
