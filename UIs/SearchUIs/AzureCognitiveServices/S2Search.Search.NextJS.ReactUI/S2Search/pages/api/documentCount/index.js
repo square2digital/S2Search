ac748e63870c31495c2d17f8f4c9d060f827f57c
jonathan gilmartin <jonathangilmartin@square2digital.com>
2025-10-26 14:14:58 +0000

    Add cancellation parameter to API handler functions

--- UIs/SearchUIs/AzureCognitiveServices/S2Search.Search.NextJS.ReactUI/S2Search/pages/api/documentCount/index.js
+++ UIs/SearchUIs/AzureCognitiveServices/S2Search.Search.NextJS.ReactUI/S2Search/pages/api/documentCount/index.js
@@ -1,5 +1,5 @@
 const { genericAPI } = require('../shared/genericAPI');
 
-export default async function handler(req, res) {
-  return await genericAPI(req, res, 'documentCount', '', true);
+export default async function handler(req, res, cancellation) {
+  return await genericAPI(req, res, 'documentCount', '', true, cancellation);
 }
--- UIs/SearchUIs/AzureCognitiveServices/S2Search.Search.NextJS.ReactUI/S2Search/pages/api/facet/index.js
+++ UIs/SearchUIs/AzureCognitiveServices/S2Search.Search.NextJS.ReactUI/S2Search/pages/api/facet/index.js
@@ -1,5 +1,5 @@
 const { genericAPI } = require('../shared/genericAPI');
 
-export default async function handler(req, res) {
-  return await genericAPI(req, res, 'facets', '', false);
+export default async function handler(req, res, cancellation) {
+  return await genericAPI(req, res, 'facets', '', false, cancellation);
 }
