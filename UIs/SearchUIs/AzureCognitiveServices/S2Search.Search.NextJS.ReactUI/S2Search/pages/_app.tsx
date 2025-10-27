6fbaad36e6fe4738b5edf7ad31f08ec5917b7897
jonathan gilmartin <jonathangilmartin@square2digital.com>
2025-10-27 09:54:08 +0000

    refactor: Integrate Redux and ThemeProvider into _app.tsx for improved state management and theming

--- UIs/SearchUIs/AzureCognitiveServices/S2Search.Search.NextJS.ReactUI/S2Search/pages/_app.tsx
+++ UIs/SearchUIs/AzureCognitiveServices/S2Search.Search.NextJS.ReactUI/S2Search/pages/_app.tsx
@@ -6,15 +6,35 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import CssBaseline from '@mui/material/CssBaseline';
 import Head from 'next/head';
+import { Provider as ReduxProvider } from 'react-redux';
+import { store } from '../store';
+import { ThemeProvider } from '@mui/material/styles';
+import { DefaultTheme } from '../common/Constants';
+import { createAppTheme } from '../common/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const theme = createAppTheme({
+    primaryColor: DefaultTheme.primaryHexColour,
+    secondaryColor: DefaultTheme.secondaryHexColour,
+  });
+
   return (
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+        {/* Default meta tags - can be overridden by individual pages */}
+        <meta
+          name="description"
+          content="S2 Search - Modern search interface"
+        />
       </Head>
-      <CssBaseline />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <ReduxProvider store={store}>
+          <Component {...pageProps} />
+        </ReduxProvider>
+      </ThemeProvider>
     </>
   );
 }
--- UIs/SearchUIs/AzureCognitiveServices/S2Search.Search.NextJS.ReactUI/S2Search/pages/index.tsx
+++ UIs/SearchUIs/AzureCognitiveServices/S2Search.Search.NextJS.ReactUI/S2Search/pages/index.tsx
@@ -2,37 +2,19 @@ import React from 'react';
 import Head from 'next/head';
 import VehicleSearchApp from '@/components/VehicleSearchApp';
 import ResetFacets from '@/common/functions/ResetFacets';
-import { Provider as ReduxProvider } from 'react-redux';
-import { store } from '../store';
-import { ThemeProvider } from '@mui/material/styles';
 import Container from '@mui/material/Container';
-import { DefaultTheme } from '../common/Constants';
-import { createAppTheme } from '../common/theme';
 
 const Home: React.FC = () => {
-  const theme = createAppTheme({
-    primaryColor: DefaultTheme.primaryHexColour,
-    secondaryColor: DefaultTheme.secondaryHexColour,
-  });
-
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <Head>
         <title>S2 Search</title>
-        <meta
-          name="description"
-          content="S2 Search - Modern search interface"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <ReduxProvider store={store}>
-        <Container disableGutters maxWidth={false}>
-          <VehicleSearchApp />
-          <ResetFacets />
-        </Container>
-      </ReduxProvider>
-    </ThemeProvider>
+      <Container disableGutters maxWidth={false}>
+        <VehicleSearchApp />
+        <ResetFacets />
+      </Container>
+    </>
   );
 };
 
