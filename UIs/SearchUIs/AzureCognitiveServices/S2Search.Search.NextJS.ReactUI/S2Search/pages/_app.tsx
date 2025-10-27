e70f9ca6950948c9f520c37207109cfb21915f78
jonathan gilmartin <jonathangilmartin@square2digital.com>
2025-10-27 09:47:14 +0000

    refactor: Remove createEmotionCache and related logic from _app.tsx for simplification

--- UIs/SearchUIs/AzureCognitiveServices/S2Search.Search.NextJS.ReactUI/S2Search/pages/_app.tsx
+++ UIs/SearchUIs/AzureCognitiveServices/S2Search.Search.NextJS.ReactUI/S2Search/pages/_app.tsx
@@ -1,47 +1,20 @@
 import type { AppProps } from 'next/app';
-import type { EmotionCache } from '@emotion/react';
 import '@/styles/globals.css';
 // Optimized font loading - only load weights we actually use
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { CacheProvider } from '@emotion/react';
 import CssBaseline from '@mui/material/CssBaseline';
-import createEmotionCache from '../common/createEmotionCache';
 import Head from 'next/head';
 
-// Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache();
-
-interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache;
-}
-
-export default function App(props: MyAppProps) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <CacheProvider value={emotionCache}>
+    <>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
-        {/* Preload critical fonts */}
-        <link
-          rel="preload"
-          href="/fonts/roboto-v30-latin-regular.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/roboto-v30-latin-500.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
       </Head>
       <CssBaseline />
       <Component {...pageProps} />
-    </CacheProvider>
+    </>
   );
 }
