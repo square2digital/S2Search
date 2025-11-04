3a7f9dcd3337cc6006f4582c63491c154d0357a1
Jonathan Gilmartin <jonathangilmartin@square2digital.com>
2025-11-04 20:00:26 +0000

    feat: move service principal variables to variables.tf and update resource group location

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,28 +9,6 @@ provider "azurerm" {
   resource_provider_registrations = "none"
 }
 
-variable "serviceprinciple_client_id" {
-  type        = string
-  description = "Client ID for the service principal"
-  sensitive   = true
-}
-variable "serviceprinciple_client_secret" {
-  type        = string
-  description = "Client secret for the service principal"
-  sensitive   = true
-}
-variable "tenant_id" {
-  type        = string
-  description = "Azure tenant ID"
-  sensitive   = true
-}
-variable "subscription_id" {
-  type        = string
-  description = "Azure subscription ID"
-  sensitive   = true
-}
-
-
 resource "azurerm_resource_group" "s2search_test" {
   name     = "s2search-terraform-test-rg"
   location = var.location
