912833575aabc74383fae3a394952e68e3ce4886
Jonathan Gilmartin <jonathangilmartin@square2digital.com>
2025-11-04 22:24:47 +0000

    feat: update provider version constraints for azurerm and random in Terraform configuration

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.6"
+      version = ">= 4.6"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.1"
+      version = ">= 3.1"
     }
   }
   required_version = ">= 1.8"
