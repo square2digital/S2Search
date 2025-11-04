01f1bec6167799d2b74589dae797b52eb65310f6
Jonathan Gilmartin <jonathangilmartin@square2digital.com>
2025-11-04 19:21:30 +0000

    feat: refactor main.tf to use variables for Azure service principal credentials

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,9 +1,30 @@
 provider "azurerm" {
   features {}
 
-  client_id       = "serviceprinciple.client_id"
-  client_secret   = "serviceprinciple.client_secret"
-  tenant_id       = "serviceprinciple.tenant_id"
-  subscription_id = "serviceprinciple.subscription_id"
+  client_id       = var.serviceprinciple_client_id
+  client_secret   = var.serviceprinciple_client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 
+}
+
+variable "serviceprinciple_client_id" {
+  type        = string
+  description = "Client ID for the service principal"
+  sensitive   = true
+}
+variable "serviceprinciple_client_secret" {
+  type        = string
+  description = "Client secret for the service principal"
+  sensitive   = true
+}
+variable "tenant_id" {
+  type        = string
+  description = "Azure tenant ID"
+  sensitive   = true
+}
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+  sensitive   = true
 }
