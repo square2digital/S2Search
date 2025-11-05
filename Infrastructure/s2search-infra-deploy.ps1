eaf6fce1019f400bec23092bbbf8bfa9dbc4714b
Jonathan Gilmartin <jonathangilmartin@square2digital.com>
2025-11-05 15:53:42 +0000

    feat: enhance deployment script with asset upload functionality

--- Infrastructure/s2search-infra-deploy.ps1
+++ Infrastructure/s2search-infra-deploy.ps1
@@ -6,7 +6,7 @@
 #######################
 # Simple deployment
 #######################
-# cls; cd "E:\github\S2Search\Infrastructure"; .\s2search-infra-deploy.ps1 -deployInfra $true
+# cls; cd "E:\github\S2Search\Infrastructure"; .\s2search-infra-deploy.ps1 -destroyInfra $false -deployInfra $false -uploadAssets $true
 
 # Steps
 # 1 - run Terraform to create AKS cluster and supporting infra
@@ -15,7 +15,9 @@
 # 4 - run Helm deployment script to deploy S2 Search components to AKS
 
 param (
-    [bool]$deployInfra = $false
+    [bool]$deployInfra = $false,
+    [bool]$destroyInfra = $false,
+    [bool]$uploadAssets = $false
 )
 
 function Write-Color([String[]]$Text, [ConsoleColor[]]$Color) {
@@ -36,11 +38,73 @@ $S2SearchAsciiArt = @"
 
 Write-Color -Text "$S2SearchAsciiArt" -Color DarkBlue
 
-if ($deployInfra) {
+$tfOutput = ""
 
-    Write-Color -Text "Changing to Terraform directory" -Color DarkYellow
-    cd "E:\github\S2Search\Terraform"
+Write-Color -Text "Changing to Terraform directory" -Color DarkYellow
+Set-Location "E:\github\S2Search\Terraform"
 
-    Write-Color -Text "run terraform validate" -Color DarkYellow
-    terraform validate
+# 1 - run Terraform to create AKS cluster and supporting infra
+if ($destroyInfra) {
+    Write-Color -Text "running terraform destroy" -Color DarkYellow
+    terraform destroy -var-file="values.tfvars" -var-file="secrets.tfvars"    
 }
+
+if ($deployInfra) {
+    Write-Color -Text "running terraform validate" -Color DarkYellow
+    terraform validate
+
+    Write-Color -Text "running terraform plan" -Color DarkYellow
+    terraform plan -var-file="values.tfvars" -var-file="secrets.tfvars"
+
+    Write-Color -Text "running terraform apply" -Color DarkYellow
+    terraform apply -var-file="values.tfvars" -var-file="secrets.tfvars" -auto-approve
+
+    $tfOutput = terraform output -json | ConvertFrom-Json
+
+    $storageAccountName = $tfOutput.storage_account_name.value
+
+    # Use these values in further automation
+    Write-Host "Storage Account: $storageAccountName"
+}
+
+if ($uploadAssets) {
+
+    Write-Color -Text "getting terraform outputs" -Color DarkYellow
+    $tfOutput = terraform output -json | ConvertFrom-Json
+
+    # output all vars    
+    <#     foreach ($key in $tfOutput.PSObject.Properties.Name) {
+        $value = $tfOutput.$key.value
+        Write-Host "$key = $value"
+    }
+ #>
+    # Set variables
+    $resourceGroup = $tfOutput.resource_group_name.value
+    $storageAccount = $tfOutput.storage_account_name.value
+    $containerName = "assets"
+    $localFolderPath = "C:\Users\square2\OneDrive - Square2 Digital\Square2 Digital\Development\Storage\assets"
+
+    # Get the storage account key
+    $accountKey = az storage account keys list `
+        --resource-group $resourceGroup `
+        --account-name $storageAccount `
+        --query "[0].value" `
+        --output tsv
+
+    # Upload files recursively
+    Get-ChildItem -Path $localFolderPath -Recurse -File | ForEach-Object {
+        $filePath = $_.FullName
+        $relativePath = $_.FullName.Substring($localFolderPath.Length + 1) -replace '\\', '/'
+
+        az storage blob upload `
+            --account-name $storageAccount `
+            --account-key $accountKey `
+            --container-name $containerName `
+            --name $relativePath `
+            --file $filePath `
+            --overwrite
+    }
+}
+
+
+# 2 - copy files to new assets folder in blob storage
